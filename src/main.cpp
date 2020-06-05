f27c7cb96ed3679dc4c418baaa3fa3864c55d717
Usman Mehmood <usmanmehmood55@gmail.com>
2020-06-05 15:49:37 +0500

    MQTT configured and integrated

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
 #include <Arduino.h>
 #include <DRV8840.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char *ssid = "Imran 2";
+const char *password = "9272imran";
+const char *mqtt_server = "192.168.100.93";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
 
 // Motor Driver IN
 unsigned const char M1RST = 19;
@@ -20,7 +31,7 @@ unsigned const char ENCODER_2A = 34;
 unsigned const char ENCODER_2B = 35;
 unsigned const char M1FAULT = 05;
 unsigned const char M2FAULT = 25;
-
+// 
 // PWM
 unsigned const char PWMchannel_0 = 0;
 unsigned const char PWMchannel_1 = 1;
@@ -53,10 +64,17 @@ int distance2 = 0;
 void encoderRead1();
 void encoderRead2();
 int getCoordinates();
+void setup_wifi();
+void callback(char *topic, byte *message, unsigned int length);
+void reconnect();
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
 
   pinMode(M1FAULT, INPUT);
   pinMode(M2FAULT, INPUT);
@@ -77,6 +95,19 @@ void setup()
 
 void loop()
 {
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000)
+  {
+    lastMsg = now;
+  }
+
+
   Serial.println();
   if (M1.getDistance() <= 5000)
   {
@@ -99,8 +130,9 @@ void loop()
   Serial.print(M2.getDistance());
 }
 
-// Use if ISR inside DRV8840.h does not run
-void IRAM_ATTR encoderRead1()
+
+
+void IRAM_ATTR encoderRead1() // Use if ISR inside DRV8840.h does not run
 {
   M1.encoderRead();
 }
@@ -113,4 +145,73 @@ void IRAM_ATTR encoderRead2()
 int getCoordinates()
 {
   
+}
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println();
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (String(topic) == "test")
+  {
+    Serial.print("Changing output to ");
+    if (messageTemp == "on")
+    {
+      Serial.println("on");
+    }
+    else if (messageTemp == "off")
+    {
+      Serial.println("off");
+    }
+  }
+}
+
+void reconnect() // Loop runs until reconnected to MQTT broker
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client"))
+    {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("test");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000); // Wait 5 seconds before retrying
+    }
+  }
 }
